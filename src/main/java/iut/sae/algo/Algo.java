26d5881ca720ec81b7e3c5e86096cb6f38744d55
Melvin <melvin.bouyssou@proton.me>
2024-06-20 20:38:01 +0200

    38simplicite.java codacy

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -1,105 +1,89 @@
 package iut.sae.algo;
 
 
-public class Algo {
+public class Algo{
+    /**
+     * Compresse une chaîne de caractères 
+     *
+     * @param in la chaîne de caractères à compresser
+     * @return la chaîne compressée
+     */
     public static String RLE(String in){
-        
-        // Cas chaîne vide
-        if(in == null || in.isEmpty()) {
-            return "";
-        }
-
-        int cpt = 0; // Compteur de caractères
-        char dernierChar = in.charAt(0); // Dernier caractère
-        StringBuilder resultat = new StringBuilder(); // Résultat
-
-        for(int i = 0; i < in.length(); i++) { // Parcours de la chaîne
-            if(in.charAt(i) == dernierChar) { // Si le caractère i est le même que le dernier
-                if (cpt >= 9) { // Si le compteur est supérieur ou égal à 9
-                    resultat.append(cpt).append(dernierChar); // On ajoute 9 et le dernier caractère au résultat
-                    cpt = 0; // On réinitialise le compteur
-                }
-                cpt++; // On incrémente le compteur 
-            } else { // Sinon
-                resultat.append(cpt).append(dernierChar); // On ajoute le compteur et le dernier caractère au résultat
-                dernierChar = in.charAt(i); // On met à jour le dernier caractère
-                cpt = 1; // On réinitialise le compteur
+        String res ="";
+        int i=0;
+        int frequence =1;
+        while(i<in.length()-1 && in!="") {
+            if (in.charAt(i)==in.charAt(i+1)){
+                frequence++;
+            }else{
+                res=res+frequence+in.charAt(i);
+                frequence=1;
             }
+            if (frequence>9) {
+                res=res+9+in.charAt(i);
+                frequence=1;
+            }
+            i++;
+        }
+        if (in!=""){
+            res=res+frequence+in.charAt(i);
         }
 
-        resultat.append(cpt).append(dernierChar); // On ajoute le dernier compteur et le dernier caractère au résultat
-        
-        return resultat.toString(); // On retourne le résultat
+        return res;
+    
     }
 
+     /**
+     * Compresse une chaîne de caractères en utilisant l'algorithme de Run-Length Encoding (RLE) de manière récursive.
+     *
+     * @param in la chaîne de caractères à compresser
+     * @param iteration le nombre d'itérations de compression
+     * @return la chaîne compressée après le nombre d'itérations spécifié
+     * @throws AlgoException si une erreur se produit lors de la compression
+     */
     public static String RLE(String in, int iteration) throws AlgoException{
-        
-        // Cas chaîne vide
-        if(in == null || in.isEmpty()) {
-            return "";
-        }
-
-        if(iteration <= 0) { // Si l'itération est inférieure ou égale à 0
-            throw new AlgoException("L'itération doit être supérieure à 0"); // On lance une exception
-        }
-
-        String resultat = in; // Résultat
-
-        for(int i = 0; i < iteration; i++) { // Parcours des itérations
-            resultat = RLE(resultat); // On applique l'algorithme RLE
-        }
-
-        return resultat; // On retourne le résultat
+        if (iteration==1){
+            return RLE(in);
+        }else{
+            return RLE(RLE(in, iteration-1));
+        } 
     }
 
+    /**
+     * Décompresse une chaîne de caractères 
+     *
+     * @param in la chaîne de caractères à décompresser
+     * @return la chaîne décompressée
+     * @throws AlgoException si une erreur se produit lors de la décompression
+     */
     public static String unRLE(String in) throws AlgoException{
-        
-        // Cas chaîne vide
-        if(in == null || in.isEmpty()) {
-            return "";
+        String res ="";
+        int i=0;
+        while(i<in.length()-1 && in!=""){
+            int nbc=Integer.parseInt(String.valueOf(in.charAt(i)));
+            for(int j= 0; j<nbc; j++){
+                res=res+in.charAt(i+1);
+            }
+            i=i+2;
         }
+        return res;
 
-        StringBuilder resultat = new StringBuilder(); // Résultat
-        
-        for(int i =0; i < in.length(); i++) { // Parcours de la chaîne
-            if(Character.isDigit(in.charAt(i))) { // Si le caractère i est un chiffre
-                int nbFois = Character.getNumericValue(in.charAt(i)); // On récupère le chiffre
-                if(nbFois >= 9){ // Si le chiffre est supérieur ou égal à 9
-                    for(int j = 0; j < 9; j++) { // On ajoute le caractère suivant au résultat 9 fois
-                        resultat.append(in.charAt(i+1)); 
-                    }
-                    for(int j = 0; j < nbFois-9; j++) { // On ajoute le caractère suivant au résultat (nbFois-9) fois
-                        resultat.append(in.charAt(i+1));
-                    }
-                } else { // Sinon si le chiffre est inférieur à 9
-                    for(int j = 0; j < nbFois; j++) { // On ajoute le caractère suivant au résultat nbFois fois
-                        resultat.append(in.charAt(i+1));
-                    }
-                }
-            } 
-        }
-
-        return resultat.toString(); // On retourne le résultat
     }
 
+    /**
+     * Décompresse une chaîne de caractères en utilisant l'algorithme de Run-Length Encoding (RLE) de manière récursive.
+     *
+     * @param in la chaîne de caractères à décompresser
+     * @param iteration le nombre d'itérations de décompression
+     * @return la chaîne décompressée après le nombre d'itérations spécifié
+     * @throws AlgoException si une erreur se produit lors de la décompression
+     */
     public static String unRLE(String in, int iteration) throws AlgoException{
-
-        // Cas chaîne vide
-        if(in == null || in.isEmpty()) {
-            return "";
+        if (iteration==1){
+            return unRLE(in);
+        }else{
+            return unRLE(unRLE(in,iteration-1));
         }
-
-        if(iteration <= 0) { // Si l'itération est inférieure ou égale à 0
-            throw new AlgoException("L'itération doit être supérieure à 0"); // On lance une exception
-        }
-
-        String resultat = in; // Résultat
-
-        for(int i = 0; i < iteration; i++) { // Parcours des itérations
-            resultat = unRLE(resultat); // On applique l'algorithme unRLE
-        }
-
-        return resultat; // On retourne le résultat
     }
 }
 
