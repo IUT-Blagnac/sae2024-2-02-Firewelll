2b11b49d04e3247a2a772e6eaabdfd3857e336f3
Melvin <melvin.bouyssou@proton.me>
2024-06-20 22:48:29 +0200

    06efficacite Codacy

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -2,88 +2,90 @@ package iut.sae.algo;
 
 
 public class Algo{
-    /**
-     * Compresse une chaîne de caractères 
-     *
-     * @param in la chaîne de caractères à compresser
-     * @return la chaîne compressée
-     */
-    public static String RLE(String in){
-        String res ="";
-        int i=0;
-        int frequence =1;
-        while(i<in.length()-1 && in!="") {
-            if (in.charAt(i)==in.charAt(i+1)){
-                frequence++;
-            }else{
-                res=res+frequence+in.charAt(i);
-                frequence=1;
-            }
-            if (frequence>9) {
-                res=res+9+in.charAt(i);
-                frequence=1;
-            }
-            i++;
-        }
-        if (in!=""){
-            res=res+frequence+in.charAt(i);
-        }
 
-        return res;
+    public static String RLE(String source) {
+
+        if (source == null || source.isEmpty()) {
+            return "";
+        }
     
-    }
-
-     /**
-     * Compresse une chaîne de caractères en utilisant l'algorithme de Run-Length Encoding (RLE) de manière récursive.
-     *
-     * @param in la chaîne de caractères à compresser
-     * @param iteration le nombre d'itérations de compression
-     * @return la chaîne compressée après le nombre d'itérations spécifié
-     * @throws AlgoException si une erreur se produit lors de la compression
-     */
-    public static String RLE(String in, int iteration) throws AlgoException{
-        if (iteration==1){
-            return RLE(in);
-        }else{
-            return RLE(RLE(in, iteration-1));
-        } 
-    }
-
-    /**
-     * Décompresse une chaîne de caractères 
-     *
-     * @param in la chaîne de caractères à décompresser
-     * @return la chaîne décompressée
-     * @throws AlgoException si une erreur se produit lors de la décompression
-     */
-    public static String unRLE(String in) throws AlgoException{
-        String res ="";
-        int i=0;
-        while(i<in.length()-1 && in!=""){
-            int nbc=Integer.parseInt(String.valueOf(in.charAt(i)));
-            for(int j= 0; j<nbc; j++){
-                res=res+in.charAt(i+1);
+        StringBuilder chaineCodee = new StringBuilder();
+        int length = source.length();
+        int nbCaracteres = 1;
+    
+        for (int i = 0; i < length; i++) {
+            
+            while (i + 1 < length && source.charAt(i) == source.charAt(i + 1)) {
+                nbCaracteres++;
+                i++;
+    
+                // Gère les cas où le nombre de caractères consécutifs atteint 9
+                if (nbCaracteres == 9) {
+                    chaineCodee.append("9").append(source.charAt(i));
+                    nbCaracteres = 0; 
+                }
             }
-            i=i+2;
+    
+            // Ajoute le nombre d'occurrences et le caractère à la chaîne encodée finale
+            chaineCodee.append(nbCaracteres).append(source.charAt(i));
+            
+            nbCaracteres = 1;
         }
-        return res;
+    
+        return chaineCodee.toString();
+    }
+    
+        
 
+    public static String RLE(String source, int iteration) throws AlgoException{
+        if (iteration < 1) {
+            throw new AlgoException("L'itération doit être >= 1");
+        }
+        
+        for (int i = 0; i < iteration; i++) {
+            source = RLE(source);
+        }
+        return source;
     }
 
-    /**
-     * Décompresse une chaîne de caractères en utilisant l'algorithme de Run-Length Encoding (RLE) de manière récursive.
-     *
-     * @param in la chaîne de caractères à décompresser
-     * @param iteration le nombre d'itérations de décompression
-     * @return la chaîne décompressée après le nombre d'itérations spécifié
-     * @throws AlgoException si une erreur se produit lors de la décompression
-     */
-    public static String unRLE(String in, int iteration) throws AlgoException{
-        if (iteration==1){
-            return unRLE(in);
-        }else{
-            return unRLE(unRLE(in,iteration-1));
+
+    public static String unRLE(String source) throws AlgoException {
+
+        if (source == null || source.isEmpty()) {
+            return "";
         }
+    
+        StringBuilder chaineDecodee = new StringBuilder();
+    
+        for (int i = 0; i < source.length(); i++) {
+           
+            char nbCaracteres = source.charAt(i);         
+            int nbLettres = nbCaracteres - '0';               
+            char lettre = source.charAt(i + 1);
+    
+            for (int j = 0; j < nbLettres; j++) {
+                chaineDecodee.append(lettre);
+            }
+    
+            i++; 
+        }
+    
+        return chaineDecodee.toString();
     }
+    
+    public static String unRLE(String source, int iteration) throws AlgoException {
+
+        if (iteration < 1) {
+            throw new AlgoException("L'itération doit être >= 1");
+        }
+    
+        for (int i = 0; i < iteration; i++) {
+            source = unRLE(source); 
+        }
+    
+        return source;
+    }
+    
+    
 }
 
