73619b12036886b00217e26a97c6b7af1e3909c9
Melvin <melvin.bouyssou@proton.me>
2024-06-21 22:04:11 +0200

    algo.h added

---
+++ analyse/algo.h
@@ -0,0 +1,9 @@
+#ifndef ALGO_H
+#define ALGO_H
+
+void RLE(char *texte);
+void unRLE(char *texte);
+void RLE_R(char *texte, int iteration);
+void unRLE_R(char *texte, int iteration);
+
+#endif // ALGO_H
